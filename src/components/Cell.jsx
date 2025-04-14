a6a7727907d1b07ef23bb94f5abb78ab05471ade
Lukas Krojer <Lukas_Krojer@reyrey.com>
2025-04-14 14:13:16 +0200

    wip

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,14 +1,7 @@
 function Cell ({ input, inputValidationStatus }) {
 
     function setInput (e) {
-
-
-        
-        console.log(e.target.value);
         input(e.target.value)        
-
-
-
         console.log(inputValidationStatus);
     }
 
