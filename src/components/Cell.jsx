ba9375682ae5340663c50d4858e4d3ef2344102a
Lukas Krojer <Lukas_Krojer@reyrey.com>
2025-04-17 16:39:08 +0200

    wip

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,12 +1,25 @@
-function Cell ({ input }) {
+import React , {useRef, useEffect} from 'react';
+
+function Cell ({ input, updateValue }) {
+    const inputRef = useRef();
 
     function setInput (e) {
         input(e.target.value)        
     }
 
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.value = ""; // clear input
+        }
+    }, [updateValue])
     return (
         <div className="cell">
-            <input onChange={setInput} className="cell-input"></input>
+            <input 
+                ref={inputRef}
+                onChange={setInput} 
+                className="cell-input"
+            >
+            </input>
         </div>
     )
 }
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,29 +3,50 @@ import Cell from "./Cell.jsx";
 
 
 function Square () {
+    // sets an array of ['', '', '', '', '', '', '', '', '']
     const [squareMatrix, setSquareMatrix] = useState(Array.from({length: 9}, () => ''));
+    const [isValid, setIsValid] = useState(true);
 
     // function handles user input from 'Cell.jsx'
-    function handleInput (value, index) {
-        // push user input to squareMatrix
+    function handleInput (input, index) {
+        // shallow copy squareMatrix array
         const updateMatrix = [...squareMatrix];
-        updateMatrix[index] = value;
-        setSquareMatrix(updateMatrix)
+
+        // square alredy contains that number
+        const isDuplicate = updateMatrix.some(
+            (elem) => elem === input // returns true or false when checking if array contains input 
+        )
+
+        if (isDuplicate) {
+            setIsValid(false)
+            return;
+        }
+        
+        // replace input at index in updateMatrix array
+        updateMatrix[index] = input;
+        // updates the squareMatrix
+        setSquareMatrix(updateMatrix);
     }
+
     
     // generates cells
     const cells = squareMatrix.map((_, index) => (
         <Cell
-            input={(value) => handleInput(value, index)}
+            input={(input) => handleInput(input, index)}
             key={index}
         />
     ));
 
+    
+
     return (
-        <div className="square" style={{ display: 'grid', gridTemplateColumns: `repeat(${3}, 1fr)`, gap: '10px' }}>
+        <div 
+            className="square" 
+            style={{ display: 'grid', gridTemplateColumns: `repeat(${3}, 1fr)`, gap: '10px' }}
+        >
             {cells}
         </div>
     )
 }
 
-export default Square 
+export default Square
